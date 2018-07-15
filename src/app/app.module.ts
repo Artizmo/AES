9192dba89f727a9b80d3362135824f6165c6482c
Artizmo <selvaggio.7@gmail.com>
2018-07-15 00:24:55 -0500

    adds aes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AesModule } from '../aes/aes.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
