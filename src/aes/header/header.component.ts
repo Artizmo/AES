bdb58bf4dfd3d9af4eb372ffd42aad65c838299c
Artizmo <selvaggio.7@gmail.com>
2018-07-20 00:26:22 -0500

    more nav work and styles

--- src/aes/header/header.component.ts
+++ src/aes/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AesService } from '../../services/aes.service';
-import { Subscriber, Subscription } from '../../../node_modules/rxjs';
+import { Subscription } from '../../../node_modules/rxjs';
+
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'aes-header',
@@ -10,13 +12,23 @@ import { Subscriber, Subscription } from '../../../node_modules/rxjs';
 export class HeaderComponent implements OnInit {
   title: string;
   titleSubscription: Subscription;
+  userMenuState: boolean = false;
+  @ViewChild(MatMenuTrigger) userMenu: MatMenuTrigger;
+
   constructor(private aes: AesService) {
     this.titleSubscription = this.aes.getPortalTitle()
       .subscribe(title => this.title = `: ${title}`);
   }
 
   ngOnInit() {
-
+    this.userMenu.onMenuClose.subscribe(() => {
+      this.userMenuState = this.userMenu.menuOpen;
+      console.log(this.userMenu)
+    })
+    this.userMenu.onMenuOpen.subscribe(() => {
+      this.userMenuState = this.userMenu.menuOpen;
+      console.log(this.userMenu)
+    })
   }
 
   test() {
